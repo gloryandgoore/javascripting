2cf17d11c694b4d6735c0d3e5b48f67de863a751
Caseyann <goore_44@hotmail.ca>
2021-11-08 23:13:01 -0500

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,6 @@
+var total = 0;
+var limit = 10;
+
+for (let i = total; i < limit; total += i) {
+console.log(total);
+}
---
+++ looping-through-arrays.js
@@ -0,0 +1,7 @@
+const pets = ['cats', 'dogs', 'rats'];
+
+for (i = 0; 2 < pets; i++) {
+  pets[i] = pets[i] + 's';
+  
+}
+console.log(pets)
